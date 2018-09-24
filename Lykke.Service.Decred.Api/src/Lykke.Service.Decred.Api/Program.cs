cde5e93eb9680901bd56b257130d28dbb0b22b55
Sef <null>
2018-09-24 19:46:48 -0400

    Use modern template + lykke.sdk

--- Lykke.Service.Decred.Api/src/Lykke.Service.Decred.Api/Program.cs
+++ Lykke.Service.Decred.Api/src/Lykke.Service.Decred.Api/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
+using Lykke.Sdk;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.PlatformAbstractions;
@@ -13,41 +14,11 @@ namespace Lykke.Service.Decred.Api
 
         public static async Task Main(string[] args)
         {
-            Console.WriteLine($"{PlatformServices.Default.Application.ApplicationName} version {PlatformServices.Default.Application.ApplicationVersion}");
-            Console.WriteLine($"ENV_INFO: {EnvInfo}");
-
-            try
-            {
-                var host = new WebHostBuilder()
-                    .UseKestrel()
-                    .UseContentRoot(Directory.GetCurrentDirectory())
-                    .UseStartup<Startup>()
-                    .UseApplicationInsights()
-                    .Build();
-
-                await host.RunAsync();
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("Fatal error:");
-                Console.WriteLine(ex);
-
-                // Lets devops to see startup error in console between restarts in the Kubernetes
-                var delay = TimeSpan.FromMinutes(1);
-
-                Console.WriteLine();
-                Console.WriteLine($"Process will be terminated in {delay}. Press any key to terminate immediately.");
-
-                await Task.WhenAny(
-                    Task.Delay(delay),
-                    Task.Run(() =>
-                    {
-                        Console.ReadKey(true);
-                        
-                    }));
-            }
-
-            Console.WriteLine("Terminated");
+#if DEBUG
+            await LykkeStarter.Start<Startup>(true);
+#else
+            await LykkeStarter.Start<Startup>(false);
+#endif
         }
     }
 }
